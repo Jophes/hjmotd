8188dd4dfc0d76eba43d36cb8feded85730f0fe9
Joseph Higgins <jophescheese@gmail.com>
2017-07-29 22:44:16 +0100

    Sync files

---
+++ resources/scripts.js
@@ -0,0 +1,34 @@
+var dropdownBars = [], containers = [], heights = [], tarHeights = [], maxHeights = [];
+
+function updateContainerHeights() {
+    for (var i = 0; i < dropdownBars.length; i++)
+    {
+        tarHeights[i] = (dropdownBars[i].className.includes(' hidden') ? 0 : maxHeights[i]);
+        heights[i] = (heights[i] + (tarHeights[i] - heights[i]) * 0.2);
+        if (Math.abs(tarHeights[i] - heights[i]) < 1) 
+            heights[i] = tarHeights[i];
+        containers[i].style = 'height: ' + heights[i] + 'px;';
+    }
+}
+
+function onLoad() {
+    dropdownBars = document.getElementsByClassName('dropdownBar');
+    for (var i = 0; i < dropdownBars.length; i++) {
+        var contentContainer = dropdownBars[i].getElementsByClassName('contentContainer')[0];
+        containers.push(contentContainer);
+        var content = contentContainer.getElementsByClassName('content')[0];
+        var maxHeight = content.offsetHeight;
+        var tarHeight = (dropdownBars[i].className.includes(' hidden') ? 0 : maxHeight);
+        contentContainer.style = 'height: ' + tarHeight + 'px;';
+        maxHeights.push(maxHeight);
+        tarHeights.push(tarHeight);
+        heights.push(tarHeight);
+        dropdownBars[i].getElementsByClassName('titleBar')[0].addEventListener('click', function(event) {
+            var parent = event.target.parentNode;
+            parent.className = (parent.className.includes(' hidden') ? parent.className.replace(' hidden', '') : parent.className + ' hidden');
+        });
+    }
+    setInterval(updateContainerHeights, 15);
+}
+
+window.addEventListener('load', onLoad);
